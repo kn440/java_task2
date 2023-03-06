94557b9e3d55e245317b9635d407b42e9b3d69e0
kn440 <78298987+kn440@users.noreply.github.com>
2023-03-06 23:25:52 +0300

    Add files via upload

---
+++ HW2_2_java_Stepanova.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.logging.*;
+public class HW2_2_java_Stepanova {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(HW2_2_java_Stepanova.class.getName());
+        FileHandler fh = new FileHandler("logTask2.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Sort Array");
+        int [] array = {2, 5, 1, -7, 3, 64, 0};
+        lib.printArrayInt(array);
+        int[] newArr = listSort(array);
+        System.out.printf("\n Отсортированный массив: \n");
+        lib.printArrayInt(newArr);
+    }
+    public static int[] listSort(int [] arr){
+        for(int k = 0; k < arr.length-1; k++) {
+            
+            for (int i = 0; i < arr.length - k-1; i++){
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    
+}
+        
